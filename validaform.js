dafc2962920afc16f24130b3f219e29c49209f62
gabrinca01 <102868270+gabrinca01@users.noreply.github.com>
2022-04-27 17:15:28 +0200

    Add files via upload

---
+++ validaform.js
@@ -0,0 +1,81 @@
+
+function controllaNome(){
+	var v = parseInt(document.myForm.nome.value);
+	if(!isNaN(v)) {
+		alert("Il nome non può essere un numero");
+		return false;
+	}
+	if(document.myForm.nome.value == ""){
+		alert("Non hai inserito il nome");
+		return false;
+	}
+	return true;
+}
+function controllaUsername(){
+	var v = parseInt(document.myForm.username.value);
+	if(!isNaN(v)) {
+		alert("L'username non può essere un numero");
+		return false;
+	}
+	if(document.myForm.username.value == ""){
+		alert("Non hai inserito l'username");
+		return false;
+	}
+	return true;
+}
+function controllaCognome(){
+	var v = parseInt(document.myForm.cognome.value);
+	if(!isNaN(v)) {
+		alert("Il cognome non può essere un numero");
+		return false;
+	}
+	if(document.myForm.cognome.value == ""){
+		alert("Non hai inserito il cognome");
+		return false;
+	}
+	return true;
+}
+function controllaEmail(){
+	var v = parseInt(document.myForm.email.value);
+	if(!isNaN(v)) {
+		alert("La email non può essere un numero");
+		return false;
+	}
+	if(document.myForm.email.value == ""){
+		alert("Non hai inserito l'email");
+		return false;
+	}
+	return true;
+}
+function controllaPassword(){
+	var v = document.myForm.password.value;
+	if(v.length < 10){
+		alert("La password deve essere lunga almeno 10 caratteri alfanumerici");
+		return false;
+	}
+	if(v == ""){
+		alert("Non hai inserito la password");
+		return false;
+	}
+	
+	return true;
+}
+function validaForm(){
+	var ret = controllaNome();
+	if(!ret)
+		return false;
+	ret =controllaCognome();
+	if(!ret)
+		return false;
+	ret = controllaUsername();
+	if(!ret)
+		return false;
+	ret = controllaEmail();
+	if(!ret)
+		return false;
+	ret = controllaPassword();
+	if(!ret)
+		return false;
+	alert("I dati sono stati inseriti correttamente");
+	return true;
+}
---
+++ validateRegistration.php
@@ -0,0 +1,46 @@
+<?php
+if (!(isset($_POST['reg-button']))) {
+    header("Location: /");
+}
+else {
+    $dbconn = pg_connect("host=localhost port=5432 dbname=PickItUpDB
+                user=postgres password=S.apienza2022") 
+                or die('Could not connect: ' . pg_last_error());
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <?php
+       
+            if ($dbconn) {
+                $email = $_POST['email'];
+                $q1="select * from users where email= $1";
+                $result=pg_query_params($dbconn, $q1, array($email));
+                if ($line=pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                    echo "<h1> Sorry, you are already a registered user</h1>
+                        ";//<a href=../login/index.html> Click here to login </a>
+                }
+                else {
+                    $nome = $_POST['nome'];
+                    $cognome = $_POST['cognome'];
+                    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                    
+                    $dataN = $_POST['birthday'];
+                    $username = $_POST['username'];
+
+                    $q2 = "insert into users(user_name,first_name,last_name,email,pswd) values ($1,$2,$3,$4,$5)";
+                    $data = pg_query_params($dbconn, $q2,
+                        array($username, $nome, $cognome, $email, $password));
+                    if ($data) {
+                        echo "<h1> Registration is completed. 
+                            Start using the website <br/></h1>";
+                        echo "<a href=../index.php?username=$username> Premi qui </a>
+                            per inziare ad utilizzare il sito web";
+                    }
+                }
+            }
+        ?> 
+    </body>
+</html>
