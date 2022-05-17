3ca4e74ab3e486f8dc62648e2b854deaf0f7c22b
DarioBusacca <dario_999@hotmail.it>
2022-05-17 16:30:50 +0200

    api key

--- Mappa/index.php
+++ Mappa/index.php
@@ -161,7 +161,7 @@
     <div id="map"></div>
 
     <script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAITpcOKXqqIpAhtxeu681KWDbGLA59NdE&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
     </script>
 </body>
 </html>
